11ec7076c82ed25ac2209d28d79d6f96a8471d95
Meg Phillips <megphillips@Megs-MacBook-Pro.local>
2022-04-24 20:59:11 -0400

    preparing the booking data to be added into the new table

--- class-booking-orders.php
+++ class-booking-orders.php
@@ -62,22 +62,64 @@ class Charter_Boat_Booking_Orders {
     protected function get_charters(){
         foreach($this->orders_by_product as $this_order){
             $order_id = $this_order->id;
+            $charter_args = array();
             foreach($this_order->line_items as $item){
                 $product_id = $item->product_id;
+                $product = wc_get_product($product_id); //get product
                 $variation_id = $item->variation_id;
-                $order_array['order_id'] = $order_id;
-                $order_array['order_status'] = $this_order->status;
-                $type = get_post_meta($item->variation_id, 'attribute_pa__cb_type', true);
-                $order_array['start_time'] = get_post_meta($item->variation_id, 'attribute_pa__cb_start_time', true);
-                $order_array['duration'] = get_post_meta($item->variation_id, 'attribute_pa__cb_duration', true);
-                $order_array['date'] = get_post_meta($item->variation_id, 'attribute_pa__cb_date', true);
-                $order_array['product_id'] = $product_id;
-                $order_array['variation_id'] = $variation_id;
-                $order_array['type'] = $type;
+                //booking fields
+                $charter_args['booking_status'] = get_post_meta($item->variation_id, 'attribute_pa__cb_type', true);
+                $date = get_post_meta($item->variation_id, 'attribute_pa__cb_date', true);
+                $start_time = get_post_meta($item->variation_id, 'attribute_pa__cb_start_time', true);
+                $charter_args['start_datetime'] = $date.' '.$start_time;
+                $charter_args['duration'] = get_post_meta($item->variation_id, 'attribute_pa__cb_duration', true);
+                $charter_args['start_location'] = get_post_meta($item->variation_id, 'attribute_pa__cb_location', true);
+                $charter_args['end_location'] = get_post_meta($item->variation_id, 'attribute_pa__cb_location', true);
+                $charter_args['tickets'] = $item->quantity;
+                $charter_args['is_private'] = $product->get_sold_individually();
+                $charter_args['customer_email'] = $this_order->billing->email;
+                $charter_args['customer_name'] = $this_order->billing->first_name.' '.$this_order->billing->last_name;
+                $charter_args['customer_phone'] = $this_order->billing->phone;
+                //booking meta to be saved
+                $charter_meta = array();
+                $charter_meta['product_id'] = $product_id;
+                $charter_meta['variation_id'] = $item->variation_id;
+                $charter_meta['order_id'] = $order_id;
+                $charter_meta['order_status'] = $this_order->status;
+                $charter_meta['order_date'] = $this_order->date_created;
+                $charter_meta['order_total'] = $this_order->total;
+                $charter_meta['booking_total'] = $item->total;
+                $charter_meta['order_discounts'] = $this_order->discount_total;
+                $charter_args['booking_meta'] = $charter_meta;
+
             }
-            $this->charters[] = $order_array;
+            $this->charters[] = $charter_args;
         }
     }
+
+    /**
+     * Get time from attribute. Add the colon between hours:mins
+     */
+    private function attribute_to_time($start_time_attribute){
+        $chunks = str_split($start_time_attribute, 2);
+        return $chunks[0].':'.$chunks[1];
+    }
+
+    /**
+    * Return Hours and Minutes from Duration
+    *
+    * basically provides the needed information for PHP DateInterval
+    *
+    * @param  string $str_duration in hours float
+    * @return array of integers
+    */
+    private function duration_float_to_time($str_duration){
+       $duration = (float)$str_duration;
+       $duration_hours = floor($duration);
+       $duration_minutes = ($duration-$duration_hours)*60;
+       return array('H'=>$duration_hours, 'M'=>$duration_minutes);
+   }
+
 
     
 
